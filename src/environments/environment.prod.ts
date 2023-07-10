cbd11a8c7346018153b295894d56ba4556e2779e
Ticonyster <85140855+Ticonyster@users.noreply.github.com>
2023-07-10 19:17:41 -0400

    added https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:'http://ec2-3-80-212-91.compute-1.amazonaws.com/qr/'
+  baseUrl:'https://ec2-3-80-212-91.compute-1.amazonaws.com/qr/'
 };
