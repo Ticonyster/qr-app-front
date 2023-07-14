bd2c16d019f685a78aa0d1ddfa7dd5d4dad9a7fc
Ticonyster <85140855+Ticonyster@users.noreply.github.com>
2023-07-14 13:44:42 -0400

    new base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:'https://aplicacion-tesis.vercel.app'
+  baseUrl:'https://qr-app-backend.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:'https://aplicacion-tesis.vercel.app'
+  baseUrl:'https://qr-app-backend.onrender.com'
 };
 
 /*
